35ab0bb9ee9bd4013dce902ca335bfd269618898
AJ <123931408+Lex-947@users.noreply.github.com>
2026-09-23 16:15:13 +0200

    Initialize EasyMagic BEER Compatibility mod class

---
+++ Neoforge/src/main/java/com/lexrei/easymagicbeer/EasyMagicBeer.java
@@ -0,0 +1,12 @@
+package com.lexrei.easymagicbeer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(EasyMagicBeer.MOD_ID)
+public class EasyMagicBeer {
+    public static final String MOD_ID = "easymagicbeer";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+}
